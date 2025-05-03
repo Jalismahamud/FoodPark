8cdae381b9b6f5cfade64cfd8e9bcc43d82e3b96
jalis <jalismahamud31@gmail.com>
2025-05-03 17:38:18 +0600

    masterign admin dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,1 +1,75 @@
-<h4>Admin dashboard</h4>
+@extends('admin.layouts.master')
+
+
+@section('content')
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-12">
+                <div class="card card-statistic-2">
+
+                    <div class="card-icon shadow-primary bg-primary">
+                        <i class="fas fa-dollar-sign"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Balance</h4>
+                        </div>
+                        <div class="card-body">
+                            $187,13
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-3 col-sm-12">
+                <div class="card card-statistic-2">
+
+                    <div class="card-icon shadow-primary bg-primary">
+                        <i class="fas fa-dollar-sign"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Balance</h4>
+                        </div>
+                        <div class="card-body">
+                            $187,13
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12">
+                <div class="card card-statistic-2">
+
+                    <div class="card-icon shadow-primary bg-primary">
+                        <i class="fas fa-shopping-bag"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Sales</h4>
+                        </div>
+                        <div class="card-body">
+                            4,732
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-12">
+                <div class="card card-statistic-2">
+
+                    <div class="card-icon shadow-primary bg-primary">
+                        <i class="fas fa-shopping-bag"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Sales</h4>
+                        </div>
+                        <div class="card-body">
+                            4,732
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+@endsection
