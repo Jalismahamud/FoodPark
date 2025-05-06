1d24aac27c63a8a351c63e7cd2a52ef46e0584da
jalis <jalismahamud31@gmail.com>
2025-05-06 19:28:38 +0600

    Fix missing newline at end of file in menu.blade.php

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -10,7 +10,7 @@ class FrontendController extends Controller
 {
     public function index()
     {
-        // return view('frontend.home.index');
-        return  view('frontend.layouts.master');
+        return view('frontend.home.index');
+        // return  view('frontend.layouts.master');
     }
 }
